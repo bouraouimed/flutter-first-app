7e9890c594cb23ea9e2f382a839ee1ce32c40c11
Mohamed Bouraoui <mbouraoui@rimads.ai>
2023-11-26 14:13:25 +0300

    cubit + bloc example

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,62 @@
-Stream<int> boatStream() async* {
-  for (int i = 0; i <= 10; i++) {
-    print("SENT BOAT Number: " + i.toString());
-    await Future.delayed(Duration(seconds: 2));
-    yield i;
+
+import 'package:bloc/bloc.dart';
+
+// class CounterCubit extends Cubit<int> {
+//   CounterCubit():super(0);
+//
+//   void increment() => emit(state +1);
+//   void decrement() => emit(state -1);
+// }
+
+enum CounterEvent {increment, decrement}
+
+class CounterBloc extends Bloc<CounterEvent, int>{
+  CounterBloc() : super(0);
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async*{
+    switch(event){
+      case CounterEvent.increment:
+        yield state +1;
+        break;
+      case CounterEvent.decrement:
+        yield state -1;
+        break;
+    }
   }
-  ;
+
+
 }
 
-void main() {
-  Stream<int> stream = boatStream();
+Future<void> main(List<String> args) async {
+  /** Cubit Testing section
+  final cubit = CounterCubit();
+  // print(cubit.state);
+  // cubit.increment();
+  // print(cubit.state);
+  // cubit.increment();
+  // cubit.increment();
+  // cubit.increment();
+  // print(cubit.state);
+  // cubit.decrement();
+  // print(cubit.state);
 
-  stream.listen((receivedData) {
-    print("RECEIVED DATA: " + receivedData.toString());
-  });
+  final streamSubscription = cubit.listen(print);
+
+  cubit.increment();
+  cubit.increment();
+  cubit.increment();
+  cubit.increment();
+  cubit.decrement(); **/
+
+  final bloc = CounterBloc();
+  final streamSubscription = bloc.listen(print);
+
+  bloc.add(CounterEvent.increment);
+  bloc.add(CounterEvent.increment);
+  bloc.add(CounterEvent.decrement);
+
+  await Future.delayed(Duration.zero);
+  streamSubscription.cancel();
+  bloc.close();
 }
