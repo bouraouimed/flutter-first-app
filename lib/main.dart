27e5dc9739b9e64b90ccfb07bce067feb91eb351
Mohamed Bouraoui <mbouraoui@rimads.ai>
2023-11-26 18:00:17 +0300

    block builder + consumer implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import './quiz.dart';
-import './result.dart';
+import 'counter_cubit.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
   }
 }
 
-class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-  var _score = 0;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-  final _questions = [
-    {
-      'questionText': 'What\'s your favorite animal?',
-      'answers': [
-        {'text': 'Rabbit', 'score': 1},
-        {'text': 'Snake', 'score': 2},
-        {'text': 'Lion', 'score': 2}
-      ]
-    },
-    {
-      'questionText': 'What\'s your favorite color?',
-      'answers': [
-        {'text': 'Yellow', 'score': 1},
-        {'text': 'Purple', 'score': 2},
-        {'text': 'Blue', 'score': 3}
-      ]
-    }
-  ];
-
-  void _resetQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _score = 0;
-    });
-  }
-
-  void _answerQuestion(int score) {
-    this._score += score;
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-  }
+  final String title;
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.isIncrement) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('That was an increment')));
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('That was a decrement')));
+                }
+              },
+              builder: (context, state) {
+                return Text(
+                  state.counterValue.toString(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  tooltip: 'Decrement',
+                  child: const Icon(Icons.remove),
+                )
+              ],
+            )
+          ],
         ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                questions: _questions,
-                answerQuestion: _answerQuestion,
-                questionIndex: _questionIndex,
-              )
-            : Result(score: _score, reset: _resetQuiz),
       ),
     );
   }
