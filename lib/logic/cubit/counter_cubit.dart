37607aa007413717c4e9acede90b4c5e78e1f0f0
Mohamed Bouraoui <mbouraoui@rimads.ai>
2023-12-04 15:48:16 +0300

    using BlocListener

--- lib/logic/cubit/counter_cubit.dart
+++ lib/logic/cubit/counter_cubit.dart
@@ -8,32 +8,11 @@ import 'internet_cubit.dart';
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  final InternetCubit internetCubit;
-
-  StreamSubscription? internetStreamSubscription;
-
-  CounterCubit({required this.internetCubit})
-      : super(CounterState(counterValue: 0, isIncrement: false)) {
-    internetStreamSubscription = internetCubit.stream.listen((internetState) {
-      if (internetState is InternetConnected &&
-          internetState.connectionType == ConnectionType.Wifi) {
-        increment();
-      } else if (internetState is InternetConnected &&
-          internetState.connectionType == ConnectionType.Mobile) {
-        decrement();
-      }
-    });
-  }
+  CounterCubit() : super(CounterState(counterValue: 0, isIncrement: false));
 
   void increment() => emit(
       CounterState(counterValue: state.counterValue + 1, isIncrement: true));
 
   void decrement() => emit(
       CounterState(counterValue: state.counterValue - 1, isIncrement: false));
-
-  @override
-  Future<void> close() {
-    internetStreamSubscription!.cancel();
-    return super.close();
-  }
 }
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -19,78 +19,89 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            BlocBuilder<InternetCubit, InternetState>(
-                builder: (context, state) {
-              if (state is InternetConnected &&
-                  state.connectionType == ConnectionType.Wifi) {
-                return Text('WIFI');
-              } else if (state is InternetConnected &&
-                  state.connectionType == ConnectionType.Wifi){
-                return Text('MOBILE');
-              } else if (state is InternetDisconnected){
-                return Text('Disconnected');
-              }
-              return CircularProgressIndicator();
-            }),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            BlocConsumer<CounterCubit, CounterState>(
-              listener: (context, state) {
-                if (state.isIncrement) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('That was an increment')));
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('That was a decrement')));
+    return BlocListener<InternetCubit, InternetState>(
+      listener: (ctx, state) {
+        if (state is InternetConnected &&
+            state.connectionType == ConnectionType.Wifi) {
+          BlocProvider.of<CounterCubit>(context).increment();
+        } else if (state is InternetConnected &&
+            state.connectionType == ConnectionType.Mobile) {
+          BlocProvider.of<CounterCubit>(context).decrement();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              BlocBuilder<InternetCubit, InternetState>(
+                  builder: (context, state) {
+                if (state is InternetConnected &&
+                    state.connectionType == ConnectionType.Wifi) {
+                  return Text('WIFI');
+                } else if (state is InternetConnected &&
+                    state.connectionType == ConnectionType.Wifi) {
+                  return Text('MOBILE');
+                } else if (state is InternetDisconnected) {
+                  return Text('Disconnected');
                 }
-              },
-              builder: (context, state) {
-                return Text(
-                  state.counterValue.toString(),
-                  style: Theme.of(context).textTheme.headlineMedium,
-                );
-              },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increment();
-                  },
-                  tooltip: 'Increment',
-                  child: const Icon(Icons.add),
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).decrement();
-                  },
-                  tooltip: 'Decrement',
-                  child: const Icon(Icons.remove),
-                )
-              ],
-            ),
-            MaterialButton(
-              color: widget.color,
-              onPressed: () {
-                Navigator.of(context).pushNamed('/second');
-              },
-              child: Text('Go to second Screen'),
-            )
-          ],
+                return CircularProgressIndicator();
+              }),
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              BlocConsumer<CounterCubit, CounterState>(
+                listener: (context, state) {
+                  if (state.isIncrement) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('That was an increment')));
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('That was a decrement')));
+                  }
+                },
+                builder: (context, state) {
+                  return Text(
+                    state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).increment();
+                    },
+                    tooltip: 'Increment',
+                    child: const Icon(Icons.add),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).decrement();
+                    },
+                    tooltip: 'Decrement',
+                    child: const Icon(Icons.remove),
+                  )
+                ],
+              ),
+              MaterialButton(
+                color: widget.color,
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/second');
+                },
+                child: Text('Go to second Screen'),
+              )
+            ],
+          ),
         ),
       ),
     );
